faca38d59878021511aa8ad1f037cbc325eab970
franlol <fraaaaan@gmail.com>
2019-08-11 18:25:48 +0200

    Updates

---
+++ precache-manifest.54ac8d5feb376d948ce0ab4bfb1a8cf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91c56c5b5e6c7c294de0045465c629b",
+    "url": "/brastlewark-gnomes-2019/index.html"
+  },
+  {
+    "revision": "e928cca4b672f33cc4be",
+    "url": "/brastlewark-gnomes-2019/static/css/main.b8f26177.chunk.css"
+  },
+  {
+    "revision": "a89eda8aab9bfebce0f1",
+    "url": "/brastlewark-gnomes-2019/static/js/2.f27a3ff0.chunk.js"
+  },
+  {
+    "revision": "e928cca4b672f33cc4be",
+    "url": "/brastlewark-gnomes-2019/static/js/main.b88fbdb7.chunk.js"
+  },
+  {
+    "revision": "2ab4fac92d8b6e549a90",
+    "url": "/brastlewark-gnomes-2019/static/js/runtime~main.e681c996.js"
+  }
+]);
